49f3f296f35f412c79095021b677ebde46747c9c
Aashish Mishra <92570822+Gowther03@users.noreply.github.com>
2024-09-27 23:36:05 +0530

    Add files via upload

---
+++ SortedSquaresOfElements.java
@@ -0,0 +1,32 @@
+package com.aurionpro.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortedSquaresOfElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the size --> ");
+		int size = scanner.nextInt();
+		int[] array = new int[size];
+		for(int i = 0;i<size;i++) {
+			System.out.print("Enter the element " + (i+1) + " --> ");
+			array[i] = scanner.nextInt();
+		}
+		squares(array);
+		Arrays.sort(array);
+		for(int i = 0;i<size;i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+	
+	public static int[] squares(int[] array) {
+		for(int i = 0;i<array.length;i++) {
+			array[i] = array[i]*array[i];
+		}
+		return array;
+	}
+
+}
